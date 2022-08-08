5f79255c059686d9c148b36ef190255662a89a19
djurayevumid <djurayevumid06@gmail.com>
2022-08-08 11:18:50 +0300

    weq
    weq

--- src/main/java/com/djurayevumid/application/model/User.java
+++ src/main/java/com/djurayevumid/application/model/User.java
@@ -1,6 +1,7 @@
 package com.djurayevumid.application.model;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "users")
@@ -71,4 +72,16 @@ public class User {
         this.salary = salary;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && salary == user.salary && Objects.equals(name, user.name) && Objects.equals(surname, user.surname) && Objects.equals(department, user.department);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, surname, department, salary);
+    }
 }
